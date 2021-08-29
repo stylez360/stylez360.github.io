67a9c644201399b06d9815af4f89b44e38e4f809
stylez360 <89502775+stylez360@users.noreply.github.com>
2021-08-29 03:23:37 -0400

    Delete feed_mon directory

--- feed_mon/feed_mon_maint.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "dbconnect.php";
-$now = time()+60;
-$timestamp_update_sql = "UPDATE `feed_status` SET `timestamp` = '$now' WHERE `uid` = '1';";
-mysqli_query($data_link, $timestamp_update_sql);
-//print "hi";
-?>
